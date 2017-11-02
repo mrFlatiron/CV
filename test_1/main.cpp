9f4511975de70220a65883b0f353550e7a7df1ad
None <mr.flatiron@mail.ru>
2017-11-02 16:32:35 +0300

    asfd

--- test_1/main.cpp
+++ test_1/main.cpp
@@ -7,30 +7,21 @@ int main(int argc, char *argv[])
 {
   (void)argc;
   (void)argv;
-  cv::Mat img_clear (124, 124, CV_8UC3);
 
-  std::string window_name = "Display window";
-  cv::namedWindow (window_name);
-  cv::imshow (window_name, img_clear);
+  cv::Mat img_clear (400, 400, CV_8UC3);
+  img_clear = cv::Scalar (0);
 
-  cv::waitKey ();
+  std::string name ("Window");
 
-  cv::Mat explosion = cv::imread ("explosion.jpg");
-
-  cv::waitKey ();
+  cv::namedWindow (name);
+  cv::imshow (name, img_clear);
+  cv::waitKey (0);
 
   cv::Point text_org (img_clear.cols / 3, img_clear.rows / 2);
-
-  cv::waitKey ();
-
-
-
   auto font_face = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
   auto font_scale = 2;
   cv::Scalar color (200, 100, 50);
   auto thickness = 3;
-
-
 
   auto countdown = 10;
   while (countdown > 0)
@@ -38,13 +29,14 @@ int main(int argc, char *argv[])
       cv::Mat img_cur = img_clear.clone ();
       std::string text = int_to_string (countdown);
       cv::putText (img_cur, text, text_org, font_face, font_scale, color, thickness, 8);
-      cv::imshow (window_name, img_cur);
+      cv::imshow (name, img_cur);
       int key_num = cv::waitKey (1000);
       if (key_num > 0)
         cv::waitKey ();
       countdown--;
     }
-  cv::imshow ("Display window", explosion);
+
   cv::waitKey ();
+
   return 0;
 }
